af14295dbe96fc8b208407b6177417a8cb905eb2
Abdul Dayan Rahat <abduddaiyan7@gmail.com>
2025-05-14 18:30:38 +0600

    Refactor Prisma setup and error handling
    - Deleted generated Prisma files to clean up the project structure.
    - Updated Prisma schema to remove unnecessary preview features and output path.
    - Implemented a centralized error handling middleware to manage errors from Prisma and Zod validation.
    - Created utility classes for API error and response handling.
    - Added async handler utility for managing asynchronous route handlers.
    - Implemented password hashing and comparison functions using bcrypt.
    - Added user validation schema using Zod for input validation.
    - Updated server setup to include error handling middleware.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 
 // import routes
 import authRoute from './routes/auth.route.js'
+import ErrorHandler from './middleware/errorHandler.js'
 
 const app = express()
 const port = process.env.PORT
@@ -16,6 +17,7 @@ app.use("/api/v1/auth",authRoute)
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
+
 
 app.listen(port, (error) => {
     if(!error) {
